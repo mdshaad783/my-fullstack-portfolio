33bef93def4ac6d428b99de5131146ee5d1ba4d8
fiamanillah <fiamanillahb2@gmail.com>
2025-02-25 19:04:03 +0600

    Cors update

--- portfolio-backend/src/app.ts
+++ portfolio-backend/src/app.ts
@@ -4,10 +4,20 @@ import errorMiddleware from './middleware/errorHandler';
 import formRoutes from './routes/formRoutes';
 const app = express();
 
+app.use(
+    cors({
+        origin: 'https://fi.amanillah.com', // Allow only your frontend domain
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true, // Allow cookies if needed
+    })
+);
+
+app.options('*', cors());
+
 // Middleware
 app.use(express.json());
 
-app.use(cors());
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
 });
