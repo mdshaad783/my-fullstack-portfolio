09db59ce135eef82c008b75f123621624da6e9d6
fiamanillah <fiamanillahb2@gmail.com>
2025-02-25 18:56:52 +0600

    Send email with template added

--- portfolio-backend/src/controllers/contactFormController.ts
+++ portfolio-backend/src/controllers/contactFormController.ts
@@ -1,22 +1,36 @@
-import { Request, Response } from 'express';
+import e, { Request, Response } from 'express';
 import sendMail from '../services/mailService';
+import path from 'path';
+import ejs from 'ejs';
 
 const contactFormController = async (req: Request, res: Response) => {
     const { name, email, message } = req.body;
 
-    const mailContent = {
-        subject: 'Portfolio Contact Form Submission',
-        message: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    };
+    const userTemplatePath = path.join(__dirname, '../emails/templates/userTemplate.ejs');
+    const adminTemplatePath = path.join(__dirname, '../emails/templates/adminTemplate.ejs');
+    const userTemplate = await ejs.renderFile(userTemplatePath, {
+        name,
+    });
+
+    const adminTemplate = await ejs.renderFile(adminTemplatePath, {
+        name,
+        email,
+        message,
+    });
 
     try {
-        const info = await sendMail(
+        const userMailInfo = await sendMail(
+            email,
+            `Thank You ${name} for Reaching Out!`,
+            userTemplate
+        );
+        const adminMailInfo = await sendMail(
             'fiamanillahb2@gmail.com',
-            mailContent.subject,
-            mailContent.message
+            'Portfolio Contact Form Submission',
+            adminTemplate
         );
 
-        res.status(200).json(info);
+        res.status(200).json({ userMailInfo, adminMailInfo });
     } catch (error) {
         res.status(500).json({ message: 'Error sending email' });
     }
--- portfolio-backend/src/services/mailService.ts
+++ portfolio-backend/src/services/mailService.ts
@@ -1,12 +1,11 @@
 import transporter from '../config/nodemailer';
-import env from '../config/env';
 
-const sendMail = async (email: string, subject: string, message: string) => {
+const sendMail = async (email: string, subject: string, htmlContent?: string) => {
     const mailOptions = {
-        from: env.SMTP_USER,
+        from: '"Fi Amanillah" <fi@amanillah.com>',
         to: email,
         subject,
-        text: message,
+        html: htmlContent || undefined, // Use HTML if provided, otherwise undefined
     };
 
     try {
@@ -18,5 +17,3 @@ const sendMail = async (email: string, subject: string, message: string) => {
 };
 
 export default sendMail;
-
-// This snippet defines a sendMail function that takes an email address, subject, and message as arguments. It creates a mailOptions object with the sender email, recipient email, subject, and message. The function then uses the nodemailer transporter object to send the email and logs a success message or error message to the console. Finally, the sendMail function is exported for use in other parts of the application.
